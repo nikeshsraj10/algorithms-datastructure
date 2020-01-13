b9dabdab2abe3ae938c0502c7f4988c065d9aed1
nikeshsraj10 <nikeshsraj@gmail.com>
2020-01-13 14:11:42 -0500

    Solution for Valid Parentheses Problem - LC Easy

--- JS/LC.js
+++ JS/LC.js
@@ -195,3 +195,41 @@ var mergeTwoLists = function(l1, l2) {
     }
     return resRef;
 };
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+*/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let complement = new Map([[')', '('], ['}', '{'], [']', '[']]);
+    if(s.length == 0)
+        return true;
+    if(s.length % 2 === 1)
+        return false;
+    let lastChar = s[s.length - 1];
+    if(lastChar == '(' || lastChar == '[' || lastChar == '{')
+        return false;
+    let stack = new Array();
+    while(s){
+        if(lastChar == '(' || lastChar == '[' || lastChar == '{'){
+            if(lastChar == complement.get(stack[stack.length - 1]))
+                stack.pop();
+            else
+                return false;
+        }
+        else
+            stack.push(lastChar);
+        s = s.slice(0, -1);
+        lastChar = s[s.length - 1];
+    }
+    
+    return true;
+};
